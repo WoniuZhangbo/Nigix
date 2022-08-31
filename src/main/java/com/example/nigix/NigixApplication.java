a35a44ef4ac0b62c83b21e3706b24e497791e63d
WoniuZhangbo <zb27338@163.com>
2022-08-31 22:20:26 +0800

    kafka

---
+++ src/main/java/com/example/nigix/NigixApplication.java
@@ -0,0 +1,13 @@
+package com.example.nigix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NigixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NigixApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/nigix/NigixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nigix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NigixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
